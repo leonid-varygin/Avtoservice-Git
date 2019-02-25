7b5e06c6d303d3483f0a153c71b4bb340f839c56
Leo Varygin <varygin.design@gmail.com>
2019-02-25 10:20:34 +0700

    Обновление политики безопасности

--- build/js/post.js
+++ build/js/post.js
@@ -1,6 +1,5 @@
-// Отправка заявки
 $(document).ready(function() {
-    $('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+    $('#form').submit(function() {
         if (document.form.name.value == '' || document.form.phone.value == '' ) {
             valid = false;
             return valid;
@@ -17,21 +16,15 @@ $(document).ready(function() {
         return false;
     });
 });
-
-// Закрыть попап «спасибо»
 $('.js-close-thank-you').click(function() { // по клику на крестик
     $('.js-overlay-thank-you').fadeOut();
 });
-
-$(document).mouseup(function (e) { // по клику вне попапа
+$(document).mouseup(function (e) {
     var popup = $('.popup');
     if (e.target!=popup[0]&&popup.has(e.target).length === 0){
         $('.js-overlay-thank-you').fadeOut();
     }
 });
-
-// Маска ввода номера телефона (плагин maskedinput)
 $(function($){
     $('[name="phone"]').mask("+7(999) 999-9999");
 });
-
