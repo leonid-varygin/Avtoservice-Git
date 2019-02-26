6fcf8ca63cefa576d0cad3bbefd8fff888fa333c
Leo Varygin <varygin.design@gmail.com>
2019-02-26 18:45:21 +0700

    Добавлен прелоадер

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    setTimeout(function () {
+        var preloader = document.getElementById('page-preloader');
+        if( !preloader.classList.contains('done') ) {
+            preloader.classList.add('done');
+        }
+    }, 1000);
+}
